220b0da63bfff799170209c660804618c27ea754
tsincomb <tsincomb@ucsd.edu>
2015-11-11 19:42:27 -0800

    Merge https://github.com/tgbugs/nlxeol
    done
    
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- neurolex_vs_nifstd.py
+++ neurolex_vs_nifstd.py
@@ -15,11 +15,20 @@ g=Graph()
 with open('cell_layer_data.csv', 'rt') as cell_open:
     cell_IDs= [r[3] for r in csv.reader(cell_open)][1:]
 print(cell_IDs)
-
+print('')
+if '_' in cell_IDs:
+    cell_IDs=cell_IDs.replace('_',' ')
 print('')
 Curie_Prefixes= v.getCuriePrefixes()[1:65]
 #Curie_Prefixes=Curie_Prefixes.sort()
-print (Curie_Prefixes)
+#print (Curie_Prefixes)
 
+'''
+for i in Curie_Prefixes:
+    for j in cell_IDs:
+        print(i,j)
+        #if v.findById(i,j)!=None:
+         #   print('hello')
 
-
+embed()
+'''
