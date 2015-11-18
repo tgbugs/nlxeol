e8b3055b1a71b93d44961b520539b6cf4d9df66c
tsincomb <tsincomb@ucsd.edu>
2015-11-17 16:56:06 -0800

    Merge https://github.com/tgbugs/nlxeol
    done
    
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- neurolex_vs_nifstd.py
+++ neurolex_vs_nifstd.py
@@ -18,14 +18,13 @@ need=0
 
 with open('cell_layer_data.csv', 'rt') as cell_open:
     cell_IDs= [r[3] for r in csv.reader(cell_open)][1:]
-    '''
 with open('brain_region_data.csv', 'rt') as brain_open:
     cell_IDs+= [r[3] for r in csv.reader(brain_open)][1:]
 with open('neuron_data_curated.csv', 'rt') as neuron_open:
     cell_IDs+= [r[3] for r in csv.reader(neuron_open)][1:]
 
 print(cell_IDs)
-'''
+
 
 
 n=len(cell_IDs)
@@ -33,19 +32,20 @@ print('We have',n,'IDs')
 #print('')
 
 Curie_Prefixes= v.getCuriePrefixes()[1:65]
+Curie_Prefixes.append('nlx_only')
 print(Curie_Prefixes)
 
 for IDs in cell_IDs:
     for Prefixes in Curie_Prefixes:
         have=(Prefixes+':'+IDs)
         have=v.findById(have)
-    if have!=None:
-        print(IDs)
-        NIF_Directory[Prefixes].append(IDs)
-        count=count+1
-        print(count)
-        break
-    else:
+        if have!=None:
+            print(IDs)
+            NIF_Directory[Prefixes].append(IDs)
+            count=count+1
+            print(count)
+            break
+    if have==None:
         NIF_Directory['nlx_only'].append(IDs)
         need=need+1
 
