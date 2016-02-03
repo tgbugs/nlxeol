3f6ac38805fa18e5dca27e26ebb051f363867372
Tom Gillespie <tgbugs@gmail.com>
2016-02-03 10:47:07 -0800

    more changes for conversion

--- tom_data_compare.py
+++ tom_data_compare.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''
 Need to DO:
 Find a way to use what I learned from common and use it to merge rows and sci respectfully and do it
@@ -7,7 +8,6 @@ Tuesday 3.5
 Th 1hr + 2:30-6:05
 '''
 
-
 import re
 import numpy
 import collections
@@ -16,31 +16,15 @@ import json
 import csv
 from collected import data #imports data from a previous py file
 from collections import defaultdict, namedtuple
+from IPython import embed
 
+g = Graph()
 
-
-count=0
-my_names={}
-meta_names={}
-pre_csv={}
-Scigraph_info={}
-coms={}
-g=Graph()
-k=[]
-finished_dict=defaultdict(list)
-labels=[]
-elements=[]
-x={}
-y=[]
-comcat={}
-temp=defaultdict(list)
-
-
-#FIXME
-#with open('cell_layer_data.csv', 'rt') as f:
-    #cell_labels= [r[0:14] for r in csv.reader(f)][0]#only 14 columns to work with
-#print(new_labels, '\n')
-
+with open('species_data.csv', 'rt') as f:
+    species_rows = [r for r in csv.reader(f)]
+    species_labels = species_rows[0]
+    species_labels[0] = 'Categories'
+    species_rows = species_rows[1:]
 with open('cell_layer_data.csv', 'rt') as f:
     layer_rows = [r for r in csv.reader(f)]
     layer_labels = layer_rows[0]
@@ -57,112 +41,134 @@ with open('neuron_data_curated.csv', 'rt') as f:
     cell_labels[0] = 'Categories'
     cell_rows = cell_rows[1:]
 
-
-scigraph = ('http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#cell_ontology_ID', 'http://www.geneontology.org/formats/oboInOwl#hasVersion',
-            'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#neuroNamesAncillaryTerm',
-            'http://purl.obolibrary.org/obo/UBPROP_0000012', 'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#birnlexDefinition', 'http://www.w3.org/2002/07/owl#deprecated',
-            'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#MeshUid',
-            'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#nifID', 'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#createdDate',
-            'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#bamsID', 'http://www.geneontology.org/formats/oboInOwl#id',
-            'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#curationStatus', 'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#isReplacedByClass',
-            'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#bonfire_ID', 'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#altDefinition', 'category',
-            'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#birnlexPendingDifferentiaNote', 'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#neuronamesID',
-            'http://purl.obolibrary.org/obo/UBPROP_0000001', 'http://www.geneontology.org/formats/oboInOwl#hasBroadSynonym', 'http://www.w3.org/2004/02/skos/core#scopeNote', 'http://www.w3.org/2004/02/skos/core#example',
-            'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#usageNote', 'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#birnlexRetiredDefinition', 'acronym',
-            'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#externalSourceURI', 'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#PMID',
-            'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#externallySourcedDefinition', 'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#pendingMereotopologicalRelationNote',
-            'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#modifiedDate', 'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#pendingActionNote', 'types',
-            'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#hasAbbrevSource', 'http://www.geneontology.org/formats/oboInOwl#hasExactSynonym', 'http://www.w3.org/2004/02/skos/core#historyNote',
-            'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#definingCitation', 'http://purl.obolibrary.org/obo/UBPROP_0000003', 'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#tempDefinition',
-            'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#externalSourceId', 'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#umls_ID',
-            'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#sao_ID', 'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#UmlsCui',
-            'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#hasCurationStatus', 'http://purl.obolibrary.org/obo/IAO_0000115', 'http://ontology.neuinfo.org/NIF/#createdDate',
-            'http://www.w3.org/2004/02/skos/core#definition', 'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#bonfireID', 'http://purl.org/dc/elements/1.1/description',
-            'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#hasExternalSource',
-            'http://www.w3.org/2002/07/owl#versionInfo', 'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#acronym',
-            'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#hasBirnlexCurator',
-            'http://www.geneontology.org/formats/oboInOwl#hasDbXref', 'http://www.w3.org/2004/02/skos/core#editorialNote',
-            'http://www.w3.org/2000/01/rdf-schema#comment', 'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#definingCitationURI',
-            'http://purl.org/dc/elements/1.1/source', 'http://www.geneontology.org/formats/oboInOwl#editor_notes',
-            'http://www.geneontology.org/formats/oboInOwl#hasRelatedSynonym', 'http://www.geneontology.org/formats/oboInOwl#creation_date',
-            'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#curator',
-            'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#abbrev',
-            'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#definitonSource', 'http://www.geneontology.org/formats/oboInOwl#hasOBONamespace',
-            'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#hasDefinitionSource',
-            'http://ontology.neuinfo.org/NIF/BiomaterialEntities/NIF-Cell.owl#sao_ID', 'http://www.w3.org/2004/02/skos/core#changeNote',
-            'http://protege.stanford.edu/plugins/owl/protege#sao_ID', 'http://purl.org/dc/elements/1.1/contributor', 'synonym',
-            'http://ontology.neuinfo.org/NIF/Backend/BIRNLex_annotation_properties.owl#hasFormerParentClass')
-
+badcats = {
+ ':Category:':'owl:Thing',
+ ':Category:Cell layer':'nlx_149357',
+ ':Category:Defined neuron class':'nlx_55759',
+ ':Category:Glial-like cell':':Category:Glial-like cell',  # UWOTM8
+ ':Category:Hemisphere parts of cerebral cortex':'birnlex_1796',
+ ':Category:Lobe parts of cerebral cortex':'birnlex_922',
+ ':Category:Magnocellular neurosecretory cell':'nlx_cell_20090501',
+ ':Category:Neuron':'sao1417703748',
+ ':Category:Organism':'birnlex_2',
+ ':Category:Regional part of brain':'birnlex_1167',
+ ':Category:Retina ganglion cell B':'BAMSC1010',
+ ':Category:Retina ganglion cell C':'BAMSC1011',
+ ':Category:Sulcus':'nlx_144078',
+ ':Category:Superior colliculus stellate neuron':'BAMSC1129',
+ ':Category:Superior colliculus wide field vertical cell':'BAMSC1125',
+ ':Category:spinal cord ventral horn interneuron V2':'nlx_cell_100207',
+}
 
 print(cell_labels)
 cli = {l:i for i, l in enumerate(cell_labels)}
 bli = {l:i for i, l in enumerate(brain_labels)}
 lli = {l:i for i, l in enumerate(layer_labels)}
+sli = {l:i for i, l in enumerate(species_labels)}
 
 cell_cats = {a[cli['Categories']]:a[cli['Id']] for a in cell_rows}
 brain_cats = {a[bli['Categories']]:a[bli['Id']] for a in brain_rows}
 layer_cats = {a[lli['Categories']]:a[lli['Id']] for a in layer_rows}
+species_cats = {a[sli['Categories']]:a[sli['Id']] for a in species_rows}
+all_cats = {}
+all_cats.update(badcats)
+all_cats.update(cell_cats)
+all_cats.update(brain_cats)
+all_cats.update(layer_cats)
+all_cats.update(species_cats)
 
+def multi_split(field):
+    if ',' in field:
+        return [all_cats[v.strip()] for v in field.split()]
+    else:
+        return all_cats[field]
+
+missing = set()
 def c_trans(row):
     try:
-        row[cli['SuperCategory']] = cell_cats[':Category:' + row[cli['SuperCategory']]]
+        row[cli['SuperCategory']] = all_cats[':Category:' + row[cli['SuperCategory']]]
+
+        row[cli['Species/taxa']] = multi_split(row[cli['Species/taxa']])
+        row[cli['Located in']] = multi_split(row[cli['Located in']])
+        row[cli['LocationOfAxonArborization']] = multi_split(row[cli['LocationOfAxonArborization']])
+        row[cli['DendriteLocation']] = multi_split(row[cli['DendriteLocation']])
+        row[cli['LocationOfLocalAxonArborization']] = multi_split(row[cli['LocationOfLocalAxonArborization']])
+
     except KeyError as e:
-        print(e)
+        missing.add(e.args[0])
+        #print(e)
     return row
 
 def b_trans(row):
     try:
-        row[bli['SuperCategory']] = brain_cats[':Category:' + row[bli['SuperCategory']]]
+        row[bli['SuperCategory']] = all_cats[':Category:' + row[bli['SuperCategory']]]
     except KeyError as e:
-        print(e)
+        missing.add(e.args[0])
+        #print(e)
     return row
 
 def l_trans(row):
     try:
-        row[lli['SuperCategory']] = layer_cats[':Category:' + row[lli['SuperCategory']]]
+        row[lli['SuperCategory']] = all_cats[':Category:' + row[lli['SuperCategory']]]
     except KeyError as e:
-        print(e)
+        missing.add(e.args[0])
+        #print(e)
+    return row
+
+def s_trans(row):
+    try:
+        row[sli['SuperCategory']] = all_cats[':Category:' + row[lli['SuperCategory']]]
+    except KeyError as e:
+        missing.add(e.args[0])
+        #print(e)
     return row
 
 cell_rows_dict = {a[cli['Id']]:c_trans(a) for a in cell_rows}
 brain_rows_dict = {a[bli['Id']]:b_trans(a) for a in brain_rows}
 layer_rows_dict = {a[lli['Id']]:l_trans(a) for a in layer_rows}
+species_rows_dict = {a[sli['Id']]:s_trans(a) for a in species_rows}
+print(missing)
 #print(cell_rows_dict.keys())
 
+def add_elements(record, rows, labels, id_):
+    if id_ in rows:
+        for label, element in zip(labels, rows[id_]):
+            if element:
+                record[label] = element
 
 output = {}
-missing = []
+scigraph = set()
 for prefix, outer_identifiers in data.items():
-        for outer_identifier in outer_identifiers:
-            record = []
-            if outer_identifier in cell_rows_dict:
-                record.extend(cell_rows_dict[outer_identifier])
-            elif outer_identifier in brain_rows_dict:
-                record.extend(brain_rows_dict[outer_identifier])
-            elif outer_identifier in layer_rows_dict:
-                record.extend(layer_rows_dict[outer_identifier])
-            else:
-                raise TypeError('WHAT ????????????????')
-                #missing.append(outer_identifier)
-                #record.extend(['' for a in new_labels])
+    for outer_identifier in outer_identifiers:
+        record = {}
+        # right now DNE duplicate rows
+        record['curie'] = prefix + ':' + outer_identifier
+        if outer_identifier in cell_rows_dict:
+            add_elements(record, cell_rows_dict, cell_labels, outer_identifier)
+        elif outer_identifier in brain_rows_dict:
+            add_elements(record, brain_rows_dict, brain_labels, outer_identifier)
+        elif outer_identifier in layer_rows_dict:
+            add_elements(record, layer_rows_dict, layer_labels, outer_identifier)
+        else:
+            raise TypeError('WHAT ????????????????')
+            #missing.append(outer_identifier)
+            #record.extend(['' for a in new_labels])
 
-            if prefix != 'nlx_only':
-                node = g.getNode(prefix + ':' + outer_identifier)
-                metadict = node['nodes'][0]['meta']
-                for key in scigraph:
-                    record.append(metadict.get(key, ''))
-            else:
-                record.extend(['' for a in scigraph])
-            output[outer_identifier] = record
+        if prefix != 'nlx_only':
+            node = g.getNode(record['curie'])
+            metadict = node['nodes'][0]['meta']
+            for key, value in metadict.items():
+                record[key] = value
+                scigraph.add(key)
+
+        output[outer_identifier] = record
 
                 #for key, value in node['nodes'][0]['meta'].items():#pulls items info out of web
 
 #FIXME IMPORTANT
 #adds a list of the order of which all the info is in under the key 'LABEL'
 #finished_dict['LABELS'].append(csv_sci)
-output['LABELS'] = cell_labels + list(scigraph)
-print(missing)
-
+output['LABELS'] = sorted(set(cell_labels + brain_labels + layer_labels + list(scigraph)))
 
 
 #FIXME
