f1dc260fa5a3f97b5e61da62b618cf3eddaca6eb
Tom Gillespie <tgbugs@gmail.com>
2016-07-18 14:52:17 -0700

    first pass at dealing with/id/category issues

--- process_csv.py
+++ process_csv.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
 
+import rdflib
 from pyontutils.utils import makeGraph, rowParse
 from IPython import embed
 import csv
 
 class convertCurated(rowParse):
     def __init__(self, graph, rows, header):
-        super().__init__(rows, header)#, order=[0])
+        self.graph = graph
+        self.set_LocationOfAxonArborization = set()
+        self.cat_id_dict = {}
+        self.fake_url_prefix = 'http://fake.org/'
+        eval_first = ['Id', 'Category']
+        super().__init__(rows, header, order=eval_first)
+
+    def _translate_category_id(self, category):  # FIXME this fails when referencing later defined cats
+        if category in self.cat_id_dict:
+            return self.cat_id_dict[category]
+        else:
+            return category
+
+    def Category(self, value):
+        self.category = self.fake_url_prefix + value.replace(' ','_')
+        if self.id_ is None:
+            self.Id(self.category)
+        self.cat_id_dict[self.category] = self.id_
 
     def Label(self, value):
         #print(value)
@@ -15,8 +33,13 @@ class convertCurated(rowParse):
         #print(value)
         pass
     def Id(self, value):
-        #print(value)
-        pass
+        if not value:
+            self.id_ = None
+        else:
+            self.id_ = self.fake_url_prefix + value  # TODO need proper curie prefixes
+            self.graph.add_node(self.id_, rdflib.RDF.type, rdflib.OWL.Class)
+        print(value)
+
     def PMID(self, value):
         #print(value)
         pass
@@ -25,7 +48,10 @@ class convertCurated(rowParse):
         pass
     def SuperCategory(self, value):
         print(value)
-        #pass
+        value = self.fake_url_prefix + ':Category:' + value.replace(' ','_')
+        value = self._translate_category_id(value)  # FIXME out of order issues
+        self.graph.add_node(self.id_, rdflib.RDFS.subClassOf, value)
+
     def Species_taxa(self, value):
         #print(value)
         pass
@@ -75,7 +101,25 @@ class convertCurated(rowParse):
         #print(value)
         pass
     def LocationOfAxonArborization(self, value): 
-        #print(value)
+        if value:
+            print(value)
+        NONE = 'None'
+        fixes = {
+            ':Category:NA':NONE,
+            ':Category:None':NONE,
+            ':Category:No axon':NONE,
+        }
+        if value in fixes:
+            value = fixes[value]
+
+        self.set_LocationOfAxonArborization.add(value)
+
+        to_skip = {':Category:NA', ':Category:None', ':Category:No axon', ''}
+        if value in to_skip:
+            pass
+        else:
+            #put_the_value_in_the_graph(value)
+            self.graph.add_node(self.category, 'http://LocationOfAxonArborization.org', value)
         pass
     def LocationOfLocalAxonArborization(self, value): 
         #print(value)
@@ -86,7 +130,11 @@ class convertCurated(rowParse):
     def OriginOfAxon(self, value): 
         #print(value)
         pass
-    def Neurotransmitter_NeurotransmitterReceptors(self, value): 
+    def Neurotransmitter(self, value):
+        #if value:
+            #print(value)
+        pass
+    def NeurotransmitterReceptors(self, value): 
         #print(value)
         pass
     def MolecularConstituents(self, value): 
@@ -105,18 +153,22 @@ class convertCurated(rowParse):
 
 def main():
     filename = 'hello world'
-    PREFIXES = {'to':'do'}
+    PREFIXES = {'to':'do','NLX':'http://fake.org/:Category:'}
     new_graph = makeGraph(filename, PREFIXES)
     rows = None  #TODO look at line 15 of nlxeol/mysqlinsert.py for this
     with open('neuron_data_curated.csv', 'rt') as f:
         rows = [r for r in csv.reader(f)]
     header = [h.replace(' ', '_')  for h in rows[0]]  #TODO
     #embed()
+    header[header.index('')] = 'Category'
     header[header.index('Species/taxa')] = 'Species_taxa'
-    header[header.index('Neurotransmitter/NeurotransmitterReceptors')] = 'Neurotransmitter_NeurotransmitterReceptors'
+    #header[header.index('Neurotransmitter/NeurotransmitterReceptors')] = 'Neurotransmitter_NeurotransmitterReceptors'
     header[header.index('Phenotypes:_ilx:has_location_phenotype')] = 'Phenotypes'
     # convert the header names so that ' ' is replaced with '_'
-    convertCurated(new_graph, rows, header)
+    state = convertCurated(new_graph, rows, header)
+    [print(i) for i in sorted(state.set_LocationOfAxonArborization)]
+    new_graph.write()
+
 
 if __name__ == '__main__':
     main()
