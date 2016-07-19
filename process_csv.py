3de50c1f5ebf0be069de69e687bdd4a2cc438f8d
Tom Gillespie <tgbugs@gmail.com>
2016-07-18 21:13:13 -0700

    updated process_csv to use fragment_curie.json and inverted the parsing order so that category comes first (the end result is exactly the same), also started running it on neurolex_full.csv

--- process_csv.py
+++ process_csv.py
@@ -3,20 +3,28 @@
 import csv
 import rdflib
 from pyontutils.utils import makeGraph, rowParse
+from pyontutils.scigraph_client import Cypher
 from IPython import embed
+import json
+
+prefixes = Cypher().getCuries()
+with open('curie_fragment.json', 'rt') as f:
+    fragment_curie_dict = json.load(f)
 
 class convertCurated(rowParse):
     def __init__(self, graph, rows, header):
         self.graph = graph
         self.set_LocationOfAxonArborization = set()
         self.chebi_ids = set()
+        self.bad_ids = set()
         self.failed_resolution = set()
 
+        self.line = 0
         self.cat_id_dict = {}
         self.to_call = []
         self.fake_url_prefix = 'ILX:'
         self.neurolex_url = 'http://neurolex.org/wiki/'
-        eval_first = ['FBbt_Id', 'Id', 'Category']
+        eval_first = ['FBbt_Id', 'Categories', 'Id']
         super().__init__(rows, header, order=eval_first)
         [func(*args) for func, args in self.to_call]
 
@@ -34,13 +42,13 @@ class convertCurated(rowParse):
         else:
             self.graph.add_node(s, p, o)
 
-    def Category(self, value):
-        if 'Resource' in value:
+    def Categories(self, value):  # FIXME called Categories in neurolex_full.csv :/
+        self.line += 1
+        if ':Category:Resource:' in value or value == 'Categories': 
             raise self.SkipError
+        elif not value:
+            raise BaseException('Category is empyt, this should not be possible! Row = %s' % self.line)
         self.category = self.neurolex_url + value.replace(' ','_')
-        if self.id_ is None:
-            self.Id(self.category)
-        self.cat_id_dict[self.category] = self.id_
 
     def Label(self, value):
         if value:
@@ -70,7 +78,8 @@ class convertCurated(rowParse):
 
     def Id(self, value):
         if not value:
-            self.id_ = None
+            self.id_ = self.category
+
         else:
             if value.startswith('JAX:') or value.startswith('FMAID:'):
                 value = value.replace(' ','')  # FIXME
@@ -84,13 +93,24 @@ class convertCurated(rowParse):
                 self.chebi_ids.add(value.replace('_',':'))
                 raise self.SkipError
 
-            if value.startswith('http://'):  # category round
+            if value in fragment_curie_dict:
+                self.id_ = fragment_curie_dict[value]
+                if self.id_ == 'NLXONLY':
+                    self.id_ += ':' + value
+                else:
+                    prefix, fragment = self.id_.split(':')
+                    if prefix not in self.graph.namespaces:
+                        self.graph.namespaces[prefix] = rdflib.Namespace(prefixes[prefix])
+                        self.graph.g.namespace_manager.bind(prefix, prefixes[prefix])
+            elif value.startswith('http://'):  # coming from category
                 self.id_ = value
             else:
-                self.id_ = self.fake_url_prefix + value  # TODO need proper curie prefixes
+                self.bad_ids.add(value)
+                raise self.SkipError('SKIPPING RECORD DUE TO wtf? %s' % value)
 
-            self.graph.add_node(self.id_, rdflib.RDF.type, rdflib.OWL.Class)
-        #print(value)
+        self.cat_id_dict[self.category] = self.id_
+        self.graph.add_node(self.id_, rdflib.RDF.type, rdflib.OWL.Class)
+
 
     def PMID(self, value):
         #print(value)
@@ -101,8 +121,8 @@ class convertCurated(rowParse):
 
     def SuperCategory(self, value):
         value = self.neurolex_url + ':Category:' + value.replace(' ','_')
-        if 'University' in value:
-            print(value)
+        #if 'University' in value:
+            #print(value)
         self._add_node(self.id_, rdflib.RDFS.subClassOf, value)
 
     def Species_taxa(self, value):
@@ -220,11 +240,11 @@ class convertCurated(rowParse):
 def main():
     filename = 'hello world'
     PREFIXES = {'to':'do',
+                'NLXONLY':'http://neurolex.org/wiki/',
                 'ILX':'http://uri.interlex.org/base/ilx_',
                 'OBOANN':'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#',  # FIXME needs to die a swift death
                }
     new_graph = makeGraph(filename, PREFIXES)
-    rows = None  #TODO look at line 15 of nlxeol/mysqlinsert.py for this
     #with open('neuron_data_curated.csv', 'rt') as f:
     with open('neurolex_full.csv', 'rt') as f:
         rows = [r for r in csv.reader(f)]
@@ -236,7 +256,10 @@ def main():
     #header[header.index('Phenotypes:_ilx:has_location_phenotype')] = 'Phenotypes'
 
     state = convertCurated(new_graph, rows, header)
-    _ = [print(i) for i in sorted(state.failed_resolution)]
+    #embed()
+    _ = [print(i) for i in sorted(state.chebi_ids)]
+    _ = [print(i) for i in sorted(state.bad_ids)]
+    #_ = [print(i) for i in sorted(state.failed_resolution)]
     #_ = [print(i) for i in sorted(state.set_LocationOfAxonArborization)]
     new_graph.write()
 
